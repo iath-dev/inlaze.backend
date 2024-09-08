f97ee1e27e73a7ee676e9ea6a211c24ce2850092
iath-dev <developer.iath@gmail.com>
2024-09-08 09:21:43 -0500

    feat(config): Adding config module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { UsersModule } from "../users/users.module";
+import { User } from "../users/user.entity";
+import { ConfigModule } from "@nestjs/config";
+import config from "src/config/config";
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "sqlite",
+      database: "db.sqlite",
+      entities: [User],
+      synchronize: true,
+    }),
+    UsersModule,
+    ConfigModule.forRoot({
+      load: [config],
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
