d098d50ec1f6ca8be51a6d08ad3ef6c484b01f9c
iath-dev <developer.iath@gmail.com>
2024-09-09 04:18:49 -0500

    feat(bug): App module config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,20 @@ import { UsersModule } from "../users/users.module";
 import { User } from "../users/user.entity";
 import { ConfigModule } from "@nestjs/config";
 import { AuthModule } from "../auth/auth.module";
+import { Favorite } from "src/favorites/favorite.entity";
+import { FavoritesModule } from "../favorites/favorites.module";
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: "sqlite",
       database: "db.sqlite",
-      entities: [User],
+      entities: [User, Favorite],
       synchronize: true,
     }),
     UsersModule,
     AuthModule,
+    FavoritesModule,
     ConfigModule.forRoot({
       isGlobal: true,
     }),
